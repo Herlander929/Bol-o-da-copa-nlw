61b591648b80ec8813b0a32fa11fdfa1585a21a6
Herlander <herlander97dasilva@gmail.com>
2022-11-06 13:21:49 -0300

    tipando svg para utilizar no react native

--- .history/src/screens/Signin_20221106131503.tsx
+++ .history/src/screens/Signin_20221106131503.tsx
@@ -6,7 +6,7 @@ import Logo from '../assets/logo.svg';
 export function SignIn(){
     return(
         <Center flex={1} bgColor="gray.900">
-       <Logo width={212} height={40}/>
+       <Logo/>
       </Center>
     )
 }
